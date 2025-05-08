337834c502c3a7cebe3ba81cdf6d940011d71b80
AlfredRenggiTay <alfredrenggitay@gmail.com>
2025-05-08 14:53:20 +0800

    Add files via upload

---
+++ pertemuan071/pertemuan071/src/pertemuan071/salam.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan071;
+
+/**
+ *
+ * @author alfred
+ */
+public class salam {
+    String tx = "menggunakan method tulisSalam tanpa parameter";
+    
+    public void tulisSalam (){
+        System.out.println(tx);
+        
+    }
+    public void tulisSalam(String txt){
+        System.out.println(txt);
+        
+    }
+    
+}
